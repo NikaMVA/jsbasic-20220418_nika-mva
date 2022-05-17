f3e115db7868a5c761d6170c6d695826df0a3159
NikaMVA <nika-mva@mail.ru>
2022-05-17 21:15:23 +0300

    Функция makeFriendsList(friends)

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  let arr = friends;
+  let str = arr.join();
+  
+  let str2 = friends.map(function(item){
+    return '<li>' + item.firstName + ' ' + item.lastName + '</li>';
+}).join('');
+  ul.insertAdjacentHTML('afterbegin', str2);
+  return ul;
 }
+
+
